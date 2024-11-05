dd211bbc7975d685a2b2b82219836418c593d6a7
Mario Fabado <mgomezdev1@gmail.com>
2024-11-05 20:07:06 +0100

    Implemented IEffector interface on Effector component

--- Assets/Scripts/Objects/Effector.cs
+++ Assets/Scripts/Objects/Effector.cs
@@ -8,24 +8,48 @@ public class EffectResult
 
 public interface IEffector
 {
-    public Vector2Int Cell { get; set; }
+    public Vector2Int Cell { get; }
     public EffectResult OnEntityEnter(CellEntity entity);
     public EffectResult OnEntityExit(CellEntity entity);
 }
 
 [RequireComponent(typeof(CellElement))]
-public class Effector : MonoBehaviour
+public class Effector : MonoBehaviour, IEffector
 {
     [SerializeField] private bool stopFall;
     [SerializeField] private List<Transform> effectorMoves;
     [SerializeField] private bool allowMovingThroughWalls = false;
 
     private CellElement cellElement;
+    private CellElement CellElement { 
+        get { 
+            if (cellElement == null) cellElement = GetComponent<CellElement>();
+            return cellElement;
+        }
+    }
+
+    public Vector2Int Cell => CellElement.Cell;
+
+    public EffectResult OnEntityEnter(CellEntity entity)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public EffectResult OnEntityExit(CellEntity entity)
+    {
+        throw new System.NotImplementedException();
+    }
 
     private void Start()
     {
         cellElement = GetComponent<CellElement>();
+        cellElement.Grid.RegisterEffector(this);
     }
 
-    
+    private void OnDestroy()
+    {
+        CellElement.Grid.DeregisterEffector(this);
+    }
+
+
 }
